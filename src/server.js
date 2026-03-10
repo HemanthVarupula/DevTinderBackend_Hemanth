5a3fe8b653e297a6efe11d05761a14a19d0dbd8d
Hemanth Varupula <varupulahemanth@gmailo.com>
2026-03-10 11:00:24 +0530

    added path in server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app=express();
 const cors=require("cors")
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://dev-tinder-web-hemanth.vercel.app",
         credentials: true,
     }
 ))
